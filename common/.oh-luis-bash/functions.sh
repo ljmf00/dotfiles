e7f2da9c70471312626a626a391040731001a23a
Luís Ferreira <contact@lsferreira.net>
2020-11-15 16:51:08 +0000

    common: bash: add function to get bash color matrix
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- common/.oh-luis-bash/functions.sh
+++ common/.oh-luis-bash/functions.sh
@@ -145,3 +145,15 @@ myip () {
 # shellcheck disable=SC2010
 lsgrep () { ls | grep "$*" ; }
 
+# get all possible bash colors matrix
+color-matrix() {
+	for x in {0..8}; do 
+	    for i in {30..37}; do 
+	        for a in {40..47}; do 
+	            echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "
+	        done
+	        echo
+	    done
+	done
+	echo ""
+}
