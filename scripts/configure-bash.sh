9d001b6b7fac29af521c881f01d5e7171af34251
Luís Ferreira <contact@lsferreira.net>
2020-11-15 16:08:11 +0000

    scripts: add a bash configurator
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ scripts/configure-bash.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if [ "$EUID" -eq 0 ]; then
+	echo "Please dont run as root"
+	exit 1
+fi
+
+CURRENT_SHELL="$(cat /etc/passwd | grep "^$(whoami):" | cut -d: -f7)"
+
+if [ "$CURRENT_SHELL" != "/bin/bash" ]; then
+	chsh -s /bin/bash "$(whoami)"
+else
+	echo "User '$(whoami)' already has /bin/bash as default shell."
+fi
