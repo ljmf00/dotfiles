390ea02f4b6e18b1a91e7241b42a784301701224
Luís Ferreira <contact@lsferreira.net>
2021-04-17 03:03:58 +0100

    deploy: add docker app for dotfiles-web using code-server
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ Dockerfile
@@ -0,0 +1,79 @@
+FROM archlinux
+LABEL maintainer="Luís Ferreira <contact at lsferreira.net>"
+
+# Add multilib repo
+RUN echo -e "[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+
+# WORKAROUND for glibc 2.33 and old Docker
+RUN patched_glibc=glibc-linux4-2.33-4-x86_64.pkg.tar.zst && \
+    curl -LO "https://repo.archlinuxcn.org/x86_64/$patched_glibc" && \
+    bsdtar -C / -xvf "$patched_glibc"
+
+# Install base-devel, git and lib32- libs for gcc runtime and glibc
+RUN pacman -Syyu --noprogressbar --needed --noconfirm \
+	base-devel git jq lib32-gcc-libs lib32-glibc
+
+# Add user, group sudo
+RUN /usr/sbin/groupadd --system sudo && \
+    /usr/sbin/useradd -m --groups sudo luis && \
+    /usr/sbin/sed -i -e "s/Defaults    requiretty.*/ #Defaults    requiretty/g" /etc/sudoers && \
+    /usr/sbin/echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+WORKDIR /home/luis/
+
+# Install yay
+RUN cd /tmp && \
+    git clone https://aur.archlinux.org/yay.git && \
+    chown -R luis:sudo yay && \
+    cd yay && \
+    sudo -u luis makepkg -sic --noprogressbar --noconfirm && \
+    cd .. && rm -rf yay
+
+# Install code-server from AUR
+RUN sudo -u luis yay -S --noprogressbar --noconfirm code-server
+
+# Use bash shell
+ENV SHELL=/usr/bin/bash
+
+# Set correct locale
+RUN echo "LC_ALL=en_US.UTF-8" >> /etc/environment && \
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && \
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+RUN locale-gen en_US.UTF-8
+ENV LC_CTYPE 'en_US.UTF-8'
+
+RUN pacman -S --noprogressbar --needed --noconfirm \
+	rclone rsync openssh python-pygments thefuck figlet fortune-mod
+
+# Remove unrequired dependencies
+RUN pacman -Rs jq --noprogressbar --noconfirm && \
+    (unused_pkgs="$(pacman -Qqdt)"; \
+    if [ "$unused_pkgs" != "" ]; then \
+        pacman -Rns $unused_pkgs --noprogressbar --noconfirm; \
+    fi )
+
+# Remove cache and update trusted certs
+RUN rm -rf /var/cache/pacman/pkg/* && \
+    rm -rf /var/lib/pacman/sync/* && \
+    rm -rf /tmp/* && \
+    trust extract-compat
+
+# Copy current context to dotfiles folder
+RUN mkdir -p /home/luis/dotfiles/
+ADD . /home/luis/dotfiles/
+
+RUN chown -R luis:sudo /home/luis/
+
+# Use our custom entrypoint script first
+RUN cp /home/luis/dotfiles/deploy/entrypoint.sh /usr/bin/deploy-container-entrypoint.sh && \
+	chmod +x /usr/bin/deploy-container-entrypoint.sh
+
+# Change to user luis
+USER luis
+
+# Port
+ENV PORT=8080
+
+# Define deploy entrypoint
+ENTRYPOINT ["/usr/bin/deploy-container-entrypoint.sh"]
---
+++ deploy/entrypoint.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+START_DIR="${START_DIR:-/home/luis}"
+
+PREFIX="dotfiles-web"
+
+mkdir -p "$START_DIR"
+
+# set hostname inside docker
+sudo hostnamectl set-hostname dotty
+
+# add rclone config and start rclone, if supplied
+if [[ -z "${RCLONE_DATA}" ]]; then
+    echo "[$PREFIX] RCLONE_DATA is not specified. Files will not persist"
+else
+    echo "[$PREFIX] Copying rclone config..."
+    mkdir -p "$HOME/.config/rclone/"
+    touch "$HOME/.config/rclone/rclone.conf"
+    echo "$RCLONE_DATA" | base64 -d > "$HOME/.config/rclone/rclone.conf"
+
+    # defasult to true
+    RCLONE_VSCODE_TASKS="${RCLONE_VSCODE_TASKS:-true}"
+    RCLONE_AUTO_PUSH="${RCLONE_AUTO_PUSH:-true}"
+    RCLONE_AUTO_PULL="${RCLONE_AUTO_PULL:-true}"
+
+    if [ $RCLONE_VSCODE_TASKS = "true" ]; then
+        # copy our tasks config to VS Code
+        echo "[$PREFIX] Applying VS Code tasks for rclone"
+        cp /tmp/rclone-tasks.json "$HOME/.local/share/code-server/User/tasks.json"
+        # install the extension to add to menu bar
+        code-server --install-extension actboy168.tasks&
+    else
+        # user specified they don't want to apply the tasks
+        echo "[$PREFIX] Skipping VS Code tasks for rclone"
+    fi
+
+    # Full path to the remote filesystem
+    RCLONE_REMOTE_PATH=${RCLONE_REMOTE_NAME:-code-server-remote}:${RCLONE_DESTINATION:-code-server-files}
+    RCLONE_SOURCE_PATH=${RCLONE_SOURCE:-$START_DIR}
+    echo "rclone sync $RCLONE_SOURCE_PATH $RCLONE_REMOTE_PATH $RCLONE_FLAGS -vv" > "$HOME/push_remote.sh"
+    echo "rclone sync $RCLONE_REMOTE_PATH $RCLONE_SOURCE_PATH $RCLONE_FLAGS -vv" > "$HOME/pull_remote.sh"
+    chmod +x "$HOME/push_remote.sh" "$HOME/pull_remote.sh"
+
+    if rclone ls $RCLONE_REMOTE_PATH; then
+
+        if [ $RCLONE_AUTO_PULL = "true" ]; then
+            # grab the files from the remote instead of running project_init()
+            echo "[$PREFIX] Pulling existing files from remote..."
+            "$HOME"/pull_remote.sh&
+        else
+            # user specified they don't want to apply the tasks
+            echo "[$PREFIX] Auto-pull is disabled"
+        fi
+
+    else
+
+        if [ $RCLONE_AUTO_PUSH = "true" ]; then
+            # we need to clone the git repo and sync
+            echo "[$PREFIX] Pushing initial files to remote..."
+            project_init
+            "$HOME"/push_remote.sh&
+        else
+            # user specified they don't want to apply the tasks
+            echo "[$PREFIX] Auto-push is disabled"
+        fi
+
+    fi
+
+fi
+
+echo "[$PREFIX] Running dotfiles configuration..."
+"$HOME"/dotfiles/configure.sh &
+
+echo "[$PREFIX] Starting code-server..."
+# Now we can run code-server with the default entrypoint
+code-server --bind-addr 0.0.0.0:8080 "$START_DIR"
