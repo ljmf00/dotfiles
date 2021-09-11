ec8afc5fff50e8532bc49562651eb832cec68ebb
Luís Ferreira <contact@lsferreira.net>
2021-09-11 03:54:37 +0100

    common: qtile: add and refactor keybindings

--- common/.config/qtile/config.py
+++ common/.config/qtile/config.py
@@ -93,6 +93,7 @@ class MouseButtons:
     NEXT = BUTTON9 = "Button9"
 
 
+# alias for keys and mouse
 k = KeysHolder()
 m = MouseButtons()
 
@@ -101,45 +102,32 @@ mod = k.SUPER
 
 terminal = guess_terminal()
 keys = [
+    # Navigation
+    # ----------
+    Key([k.SUPER], k.TAB, lazy.layout.next(),
+        desc="Move window focus to next window"),
+    Key([k.SUPER, k.SHIFT], k.TAB, lazy.layout.previous(),
+        desc="Move window focus to previous window"),
 
-    Key([mod], "space", lazy.layout.next(),
-        desc="Move window focus to other window"),
 
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
-        desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
-        desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
-        desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Appearance
+    # ----------
 
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),
-        desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Normalize sizes
+    Key([k.SUPER], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Toggle between different layouts as defined below
+    Key([k.SUPER], "l", lazy.next_layout(), desc="Toggle between layouts"),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+    Key([k.SUPER], "g", lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack"),
 
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
 
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
+    Key([k.SUPER, k.CONTROL], "F11", lazy.restart(), desc="Restart Qtile"),
+    Key([k.SUPER, k.CONTROL], "F12", lazy.shutdown(), desc="Shutdown Qtile"),
 
     # Actions for focused windows
     # ---------------------------
@@ -148,16 +136,34 @@ keys = [
     Key([k.ALT], "F4", lazy.window.kill(), desc="Kill focused window"),
 
     # Switch between windows
-    Key([mod], k.UP, lazy.layout.up(), desc="Move focus to up"),
-    Key([mod], k.DOWN, lazy.layout.down(), desc="Move focus to down"),
-    Key([mod], k.RIGHT, lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], k.LEFT, lazy.layout.left(), desc="Move focus to left"),
+    Key([k.SUPER], k.UP, lazy.layout.up(), desc="Move focus to up"),
+    Key([k.SUPER], k.DOWN, lazy.layout.down(), desc="Move focus to down"),
+    Key([k.SUPER], k.RIGHT, lazy.layout.right(), desc="Move focus to right"),
+    Key([k.SUPER], k.LEFT, lazy.layout.left(), desc="Move focus to left"),
 
-    # Vim-like shortcuts
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([k.SUPER, k.SHIFT], k.LEFT, lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([k.SUPER, k.SHIFT], k.RIGHT, lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+    Key([k.SUPER, k.SHIFT], k.DOWN, lazy.layout.shuffle_down(),
+        desc="Move window down"),
+    Key([k.SUPER, k.SHIFT], k.UP, lazy.layout.shuffle_up(), desc="Move window up"),
+
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([k.SUPER, k.CONTROL], k.LEFT, lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([k.SUPER, k.CONTROL], k.RIGHT, lazy.layout.grow_right(),
+        desc="Grow window to the right"),
+    Key([k.SUPER, k.CONTROL], k.DOWN, lazy.layout.grow_down(),
+        desc="Grow window down"),
+    Key([k.SUPER, k.CONTROL], k.UP, lazy.layout.grow_up(), desc="Grow window up"),
+
+    # Floating windows
+    Key([k.SUPER, k.SHIFT], k.SPACE, lazy.window.toggle_floating(),
+        desc="Toggle focused window to/from floating mode"),
 
 
     # Spawn applications
@@ -179,17 +185,17 @@ keys = [
 
     # Rofi
     Key(
-        [mod], "d",
+        [k.SUPER], "d",
         lazy.spawn("rofi -show run -theme menu-bar"),
         desc="Quick command runner"
     ),
     Key(
-        [mod, k.SHIFT], "d",
+        [k.SUPER, k.SHIFT], "d",
         lazy.spawn("rofi -show drun -show-icons -theme menu-center"),
         desc="Quick application launcher"
     ),
     Key(
-        [mod], "e",
+        [k.SUPER, k.SHIFT], "e",
         lazy.spawn("rofimoji"),
         desc="Quick emoji picker"
     ),
@@ -248,11 +254,11 @@ extension_defaults = widget_defaults.copy()
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
+    Drag([k.SUPER], "Button1", lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
+    Drag([k.SUPER], "Button3", lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Click([k.SUPER], "Button2", lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
