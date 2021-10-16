af4dd053db940eb6208f89f4ec65d5a724481cda
Luís Ferreira <contact@lsferreira.net>
2021-10-16 01:08:14 +0100

    common: nvim: use coq instead of compe

--- common/.config/nvim/lua/pconfig/c-lsp.lua
+++ common/.config/nvim/lua/pconfig/c-lsp.lua
@@ -2,9 +2,12 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local lspc = require('lspconfig')
+local coq = require "coq"
 
-lspc.vimls.setup{}
-lspc.clangd.setup{}
-lspc.tsserver.setup{}
-lspc.serve_d.setup{}
-lspc.jedi_language_server.setup{}
+lspc.vimls.setup(coq.lsp_ensure_capabilities({}))
+lspc.clangd.setup(coq.lsp_ensure_capabilities({}))
+lspc.tsserver.setup(coq.lsp_ensure_capabilities({}))
+lspc.serve_d.setup(coq.lsp_ensure_capabilities({}))
+lspc.jedi_language_server.setup(coq.lsp_ensure_capabilities({}))
+
+vim.cmd('COQnow -s')
