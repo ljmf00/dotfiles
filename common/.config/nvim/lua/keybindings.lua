a5894e54003144fc8e16c7b6072bc2e7c671797a
Luís Ferreira <contact@lsferreira.net>
2021-10-28 16:17:15 +0100

    common: nvim: add bufferline quick shortcuts

--- common/.config/nvim/lua/keybindings.lua
+++ common/.config/nvim/lua/keybindings.lua
@@ -158,6 +158,16 @@ local mappings = {
     { "<leader>fb", "<cmd>Telescope buffers<cr>" },
     { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
 
+    { "<leader>1", "<cmd>BufferLineGoToBuffer 1<cr>" },
+    { "<leader>2", "<cmd>BufferLineGoToBuffer 2<cr>" },
+    { "<leader>3", "<cmd>BufferLineGoToBuffer 3<cr>" },
+    { "<leader>4", "<cmd>BufferLineGoToBuffer 4<cr>" },
+    { "<leader>5", "<cmd>BufferLineGoToBuffer 5<cr>" },
+    { "<leader>6", "<cmd>BufferLineGoToBuffer 6<cr>" },
+    { "<leader>7", "<cmd>BufferLineGoToBuffer 7<cr>" },
+    { "<leader>8", "<cmd>BufferLineGoToBuffer 8<cr>" },
+    { "<leader>9", "<cmd>BufferLineGoToBuffer 9<cr>" },
+
     --  Move lines up and down
     { "<A-j>", "<cmd>m .+1<CR>=="},
     { "<A-k>", "<cmd>m .-2<CR>=="},
