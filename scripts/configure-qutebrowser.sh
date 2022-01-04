4a4ec489ac7fa23ad12709da313307918cc8f695
Luís Ferreira <contact@lsferreira.net>
2022-01-04 17:05:14 +0000

    scripts: qutebrowser: make it an optional dependency

--- scripts/configure-qutebrowser.sh
+++ scripts/configure-qutebrowser.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-if [ ! -d "$HOME/.local/share/qutebrowser/qtwebengine_dictionaries/" ]; then
-  /usr/share/qutebrowser/scripts/dictcli.py install en-US
-  /usr/share/qutebrowser/scripts/dictcli.py install en-GB
-  /usr/share/qutebrowser/scripts/dictcli.py install pt-PT
+if hash qutebrowser 2>/dev/null; then
+  if [ "$(xdg-settings get default-web-browser)" != "org.qutebrowser.qutebrowser.desktop" ]; then
+    xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop
+  fi
+
+  if [ ! -d "$HOME/.local/share/qutebrowser/qtwebengine_dictionaries/" ]; then
+    /usr/share/qutebrowser/scripts/dictcli.py install en-US
+    /usr/share/qutebrowser/scripts/dictcli.py install en-GB
+    /usr/share/qutebrowser/scripts/dictcli.py install pt-PT
+  fi
 fi
