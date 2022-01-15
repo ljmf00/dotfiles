4833330b4e52d9f6f07cc03618857b411b4c935d
Luís Ferreira <contact@lsferreira.net>
2022-01-15 04:04:48 +0000

    feat(treewide): add chromium by default
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- scripts/configure-qutebrowser.sh
+++ scripts/configure-qutebrowser.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if hash qutebrowser 2>/dev/null; then
-  if [ "$(xdg-settings get default-web-browser)" != "org.qutebrowser.qutebrowser.desktop" ]; then
-    xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop
-  fi
+  # if [ "$(xdg-settings get default-web-browser)" != "org.qutebrowser.qutebrowser.desktop" ]; then
+  #   xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop
+  # fi
 
   if [ ! -d "$HOME/.local/share/qutebrowser/qtwebengine_dictionaries/" ]; then
     /usr/share/qutebrowser/scripts/dictcli.py install en-US
