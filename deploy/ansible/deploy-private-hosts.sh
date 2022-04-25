c05e81fac260db4c968136b6b9a978c3b767b5de
Luís Ferreira <contact@lsferreira.net>
2022-04-25 23:57:25 +0100

    feat(deploy/ansible): add private servers playbook
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ deploy/ansible/deploy-private-hosts.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+trap 'rm -f "$TMP_VAULT_PASS" "$TMP_PRIVATE_HOSTS"' EXIT
+
+TMP_VAULT_PASS="$(mktemp)"    || exit 1
+TMP_PRIVATE_HOSTS="$(mktemp)" || exit 1
+
+gpg --batch --decrypt --quiet "$(dirname "$0")/private-hosts.pgp" 2>/dev/null > "$TMP_PRIVATE_HOSTS"
+"$(dirname "$0")"/get-vault-pass.sh 2>/dev/null > "$TMP_VAULT_PASS"
+
+ansible-playbook playbooks/setup-private.yml \
+  --private-key="$TMP_VAULT_PASS" \
+  --inventory-file="$TMP_PRIVATE_HOSTS" \
+  "$@"
