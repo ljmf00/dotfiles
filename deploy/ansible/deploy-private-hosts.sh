04dd0e0f0b87c5b55d0b11b92636ead2cb559c23
Luís Ferreira <contact@lsferreira.net>
2022-04-26 01:09:42 +0100

    fix(deploy/ansible): make deploy private script more secure
    This also prevents resources to leak.
    
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- deploy/ansible/deploy-private-hosts.sh
+++ deploy/ansible/deploy-private-hosts.sh
@@ -3,7 +3,9 @@
 trap 'rm -f "$TMP_VAULT_PASS" "$TMP_PRIVATE_HOSTS"' EXIT
 
 TMP_VAULT_PASS="$(mktemp)"    || exit 1
-TMP_PRIVATE_HOSTS="$(mktemp)" || exit 1
+TMP_PRIVATE_HOSTS="$(mktemp)" || (rm "$TMP_VAULT_PASS" && exit 1)
+
+chmod 600 "$TMP_VAULT_PASS" "$TMP_PRIVATE_HOSTS"
 
 gpg --batch --decrypt --quiet "$(dirname "$0")/private-hosts.pgp" 2>/dev/null > "$TMP_PRIVATE_HOSTS"
 "$(dirname "$0")"/get-vault-pass.sh 2>/dev/null > "$TMP_VAULT_PASS"
