6e5bcb1d6d251a44e0bdae745804941c8b2926a8
Luís Ferreira <contact@lsferreira.net>
2022-04-26 05:24:25 +0100

    fix(deploy/ansible): use correct inventory path and vault key
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- deploy/ansible/deploy-private-hosts.sh
+++ deploy/ansible/deploy-private-hosts.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-trap 'rm -f "$TMP_VAULT_PASS" "$TMP_PRIVATE_HOSTS"' EXIT
+trap 'rm -f "$TMP_VAULT_PASS" "$(dirname "$0")/private_hosts"' EXIT
 
-TMP_VAULT_PASS="$(mktemp)"    || exit 1
-TMP_PRIVATE_HOSTS="$(mktemp)" || (rm "$TMP_VAULT_PASS" && exit 1)
+TMP_VAULT_PASS="$(mktemp)"            || exit 1
+touch "$(dirname "$0")/private_hosts" || (rm "$TMP_VAULT_PASS" && exit 1)
 
-chmod 600 "$TMP_VAULT_PASS" "$TMP_PRIVATE_HOSTS"
+chmod 600 "$TMP_VAULT_PASS" "$(dirname "$0")/private_hosts"
 
-gpg --batch --decrypt --quiet "$(dirname "$0")/private-hosts.pgp" 2>/dev/null > "$TMP_PRIVATE_HOSTS"
+gpg --batch --decrypt --quiet "$(dirname "$0")/private-hosts.pgp" 2>/dev/null > "$(dirname "$0")/private_hosts"
 "$(dirname "$0")"/get-vault-pass.sh 2>/dev/null > "$TMP_VAULT_PASS"
 
-ansible-playbook playbooks/setup-private.yml \
-  --private-key="$TMP_VAULT_PASS" \
-  --inventory-file="$TMP_PRIVATE_HOSTS" \
+ansible-playbook \
+  --vault-password-file "$TMP_VAULT_PASS" \
+  --inventory "$(dirname "$0")/private_hosts" \
+  playbooks/setup-private.yml \
   "$@"
