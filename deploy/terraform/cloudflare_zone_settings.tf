63986552bf7a72091f9fb48c2b32df1adf992872
Luís Ferreira <contact@lsferreira.net>
2022-04-30 21:40:59 +0100

    feat(deploy/terraform): add cloudflare zone settings
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ deploy/terraform/cloudflare_zone_settings.tf
@@ -0,0 +1,49 @@
+resource "cloudflare_zone_settings_override" "lsferreira_net" {
+    zone_id = "${var.cloudflare_zone_id}"
+
+    settings {
+        # SSL/TLS
+        ssl = "strict"
+        tls_1_3 = "zrt"
+        min_tls_version = "1.3"
+
+        # HTTP(S)
+        always_use_https = "on"
+        automatic_https_rewrites = "on"
+        opportunistic_encryption = "on"
+        /* http2 = "on" # read-only */
+        http3 = "on"
+
+        # Network
+        zero_rtt = "on"
+        websockets = "on"
+
+        # IPv4/IPv6
+        pseudo_ipv4 = "off"
+        ipv6 = "on"
+
+        # Tor
+        opportunistic_onion = "on"
+
+        # Caching
+        development_mode = "off"
+        cache_level = "basic"
+        browser_cache_ttl = 14400
+        rocket_loader = "off"
+        minify {
+            css = "on"
+            js = "on"
+            html = "on"
+        }
+
+        # Compression
+        brotli = "on"
+
+        # Security
+        security_level = "high"
+        challenge_ttl = 86400
+        security_header {
+            enabled = true
+        }
+    }
+}
