b89a9bf62e21b1dc284ff665874c7e812df565a2
Luís Ferreira <contact@lsferreira.net>
2022-12-07 13:04:44 +0000

    chore(workspace/personal): use newer bootstrap method for packer
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- workspace_personal/.config/nvim/lua/plugins.lua
+++ workspace_personal/.config/nvim/lua/plugins.lua
@@ -1,14 +1,17 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
-return require('packer').startup(function()
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
   -- Packer
   use '~/dotfiles/3rdparty/vim/packer.nvim'
 
@@ -200,4 +203,8 @@ return require('packer').startup(function()
     config = function() require'pconfig.c-bufferline' end,
     requires = {'kyazdani42/nvim-web-devicons', opt = true},
   }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
