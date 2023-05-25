8a3d8ac7c60b6de03ae51a8ea5dcf9e9fd966ab7
Luís Ferreira <contact@lsferreira.net>
2023-05-25 17:11:13 +0100

    fix(common): use correct priority on gpg and ssh agent load
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- common/.oh-luis-bash/ssh.sh
+++ common/.oh-luis-bash/ssh.sh
@@ -3,9 +3,10 @@
 # shellcheck disable=SC2034
 # shellcheck disable=SC2015
 
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent -t 15m > "$XDG_RUNTIME_DIR/ssh-agent.env"
-fi
-if [[ -z ${SSH_AUTH_SOCK+x} || ! -f "$SSH_AUTH_SOCK" ]]; then
+if [[ -z ${SSH_AUTH_SOCK+x} ]]; then
+    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+        ssh-agent -t 15m > "$XDG_RUNTIME_DIR/ssh-agent.env"
+    fi
+
     source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
 fi
