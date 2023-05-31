acfdb74380310a42298c3d1ca8a7a6d2669492cc
Luís Ferreira <contact@lsferreira.net>
2023-05-31 17:42:33 +0100

    feat(treewide): use stow
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- common/.oh-luis-bash/ienv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck shell=bash
-# shellcheck disable=SC2034
-# shellcheck disable=SC2015
-
-# set $TERM variable if not set
-if [ -z ${TERM+x} ]; then
-    export TERM=xterm
-fi
