95c7f5b2ab76accd1756d564caad666eba01dc5c
Luís Ferreira <contact@lsferreira.net>
2023-06-15 02:08:35 +0100

    chore(deploy): move providers.tf to dns
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- deploy/providers.tf
+++ deploy/providers.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    cloudflare = {
-      source = "cloudflare/cloudflare"
-      version = "~> 4.8.0"
-    }
-  }
-}
-
 provider "cloudflare" {
   api_token = "${var.cloudflare_token}"
 }
