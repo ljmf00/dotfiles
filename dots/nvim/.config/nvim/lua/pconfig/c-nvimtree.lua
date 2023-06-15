27d0d0dd57eec9ca191ea6e143cfbcd1802c5918
Luís Ferreira <contact@lsferreira.net>
2023-06-15 04:19:42 +0100

    chore(dots): remove nvimtree deprecated options
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- dots/nvim/.config/nvim/lua/pconfig/c-nvimtree.lua
+++ dots/nvim/.config/nvim/lua/pconfig/c-nvimtree.lua
@@ -45,10 +45,8 @@ require'nvim-tree'.setup {
   },
   view = {
     width = 30,
-    height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = false,
     mappings = {
       custom_only = false,
       list = {}
