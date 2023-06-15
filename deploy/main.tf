8f85d64560fbf6fc15038263c0e86d4229fc6d84
Luís Ferreira <contact@lsferreira.net>
2023-06-15 02:33:22 +0100

    revert(deploy): use one folder for terraform
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- deploy/main.tf
+++
@@ -1,6 +0,0 @@
-module "dns" {
-    source = "./terraform/dns"
-    cloudflare_email = "${var.cloudflare_email}"
-    cloudflare_token = "${var.cloudflare_token}"
-    cloudflare_zone_id = "${var.cloudflare_zone_id}"
-}
--- deploy/terraform/providers.tf
+++ deploy/terraform/providers.tf
@@ -6,3 +6,7 @@ terraform {
     }
   }
 }
+
+provider "cloudflare" {
+  api_token = "${var.cloudflare_token}"
+}
