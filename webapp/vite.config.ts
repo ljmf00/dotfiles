18e0afa2609e0c07088298716c645eadc19f9ce1
Luís Ferreira <contact@lsferreira.net>
2023-07-09 21:58:45 +0100

    feat(webapp): add eslint and terser to build stage
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import manifestSRI from 'vite-plugin-manifest-sri';
 
 export default defineConfig({
     plugins: [
@@ -8,5 +10,7 @@ export default defineConfig({
             buildDirectory: 'bundle',
             refresh: true,
         }),
+        manifestSRI(),
+        ViteMinifyPlugin({}),
     ],
 });
