6eb8c6491849d845c647199862886f7012980ce2
Luís Ferreira <contact@lsferreira.net>
2023-10-03 17:13:09 +0100

    feat(nixos): add thinker host
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- flake.nix
+++ flake.nix
@@ -25,14 +25,44 @@
     };
   };
 
-  outputs = { self, home-manager, nixpkgs, nixos-generators, nur, ... }@inputs: {
-
-    # nixos installer iso
-    nixosInstallerIso = (nixpkgs.lib.nixosSystem {
+  outputs = { self, home-manager, nixpkgs, nixos-generators, nur, ... }@inputs:
+    let
       system = "x86_64-linux";
-      specialArgs = inputs;
-      modules = [ ./installer/config.nix ];
-    }).config.formats.iso;
 
-  };
+      mkSystem = pkgs: system: hostname:
+        pkgs.lib.nixosSystem {
+          system = system;
+          modules = [
+            { networking.hostName = hostname; }
+            ./nixos/modules/configuration.nix
+            (./nixos + "/hosts/${hostname}/hardware-configuration.nix")
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useUserPackages = true;
+                useGlobalPkgs = true;
+                extraSpecialArgs = { inherit inputs; };
+                users.notus = (./nixos + "/hosts/${hostname}/user.nix");
+              };
+              nixpkgs.overlays = [
+                nur.overlay
+                (import ./nixos/overlays)
+              ];
+            }
+          ];
+          specialArgs = { inherit inputs; };
+        };
+
+    in {
+      # nixos installer iso
+      nixosInstallerIso = (nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = inputs;
+        modules = [ ./nixos/installer/config.nix ];
+      }).config.formats.iso;
+
+      nixosConfigurations = {
+        thinker = mkSystem inputs.nixpkgs "x86_64-linux" "thinker";
+      };
+    };
 }
---
+++ nixos/modules/configuration.nix
@@ -0,0 +1,5 @@
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  system.stateVersion = "20.09";
+}
