91dd8a8df22783b6a5d970689305e478f50cc659
Luis Ferreira <contact@lsferreira.net>
2024-10-21 00:32:27 +0100

    chore(terraform): remove luisf.eu.org entries

--- terraform/cloudflare_argo.tf
+++ terraform/cloudflare_argo.tf
@@ -2,8 +2,3 @@ resource "cloudflare_argo" "lsferreira_net" {
   zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
   tiered_caching = "on"
 }
-
-resource "cloudflare_argo" "luisf_eu_org" {
-  zone_id = data.cloudflare_zones.luisf_eu_org.zones[0].id
-  tiered_caching = "on"
-}
