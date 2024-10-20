5745c16eab599b5570f9d2ba2743a880219adabd
Luis Ferreira <contact@lsferreira.net>
2024-10-21 00:15:09 +0100

    chore(terraform): add tags to records

--- terraform/records_email.tf
+++ terraform/records_email.tf
@@ -35,6 +35,7 @@ resource "cloudflare_record" "lsferreira_net_spf" {
   name = "lsferreira.net"
   value = "v=spf1 include:_spf.mx.cloudflare.net +ip4:193.108.130.14 +include:spf.mxyeet.net +include:_spf.lsferreira.net ~all"
   type = "TXT"
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "lsferreira_net_dmarc" {
@@ -42,6 +43,7 @@ resource "cloudflare_record" "lsferreira_net_dmarc" {
   name = "_dmarc"
   value = "v=DMARC1;p=quarantine;sp=quarantine;pct=100;rua=mailto:cdbf63edcf5a46f6a003272a9f314ef1@dmarc-reports.cloudflare.net;ruf=mailto:cdbf63edcf5a46f6a003272a9f314ef1@dmarc-reports.cloudflare.net;ri=86400;aspf=s;adkim=s;fo=0:1:d:s;"
   type = "TXT"
+  tags = [ "terraform" ]
 }
 
 # =============================================================================
@@ -54,6 +56,7 @@ resource "cloudflare_record" "lsferreira_net_dkim1" {
   value = "protonmail.domainkey.d3o6sspqrfqz73aqmw6ghj2f22kp3ob77j72axb34nj3dnhp4aqxa.domains.proton.ch"
   proxied = false
   type = "CNAME"
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "lsferreira_net_dkim2" {
@@ -62,6 +65,7 @@ resource "cloudflare_record" "lsferreira_net_dkim2" {
   value = "protonmail2.domainkey.d3o6sspqrfqz73aqmw6ghj2f22kp3ob77j72axb34nj3dnhp4aqxa.domains.proton.ch"
   proxied = false
   type = "CNAME"
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "lsferreira_net_dkim3" {
@@ -70,6 +74,7 @@ resource "cloudflare_record" "lsferreira_net_dkim3" {
   value = "protonmail3.domainkey.d3o6sspqrfqz73aqmw6ghj2f22kp3ob77j72axb34nj3dnhp4aqxa.domains.proton.ch"
   proxied = false
   type = "CNAME"
+  tags = [ "terraform" ]
 }
 
 # =============================================================================
@@ -82,6 +87,7 @@ resource "cloudflare_record" "smtp" {
   value = "lu-shared04.cpanelplatform.com"
   proxied = false
   type = "CNAME"
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "pop3" {
@@ -90,6 +96,7 @@ resource "cloudflare_record" "pop3" {
   value = "lu-shared04.cpanelplatform.com"
   proxied = false
   type = "CNAME"
+  tags = [ "terraform" ]
 }
 
 # Alias to pop3
@@ -99,6 +106,7 @@ resource "cloudflare_record" "pop" {
   value = "lu-shared04.cpanelplatform.com"
   proxied = false
   type = "CNAME"
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "imap" {
@@ -107,6 +115,7 @@ resource "cloudflare_record" "imap" {
   value = "lu-shared04.cpanelplatform.com"
   proxied = false
   type = "CNAME"
+  tags = [ "terraform" ]
 }
 
 
@@ -120,6 +129,7 @@ resource "cloudflare_record" "autoconfig" {
   value = "lu-shared04.cpanelplatform.com"
   proxied = false
   type = "CNAME"
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "autodiscover" {
@@ -128,6 +138,7 @@ resource "cloudflare_record" "autodiscover" {
   value = "lu-shared04.cpanelplatform.com"
   proxied = false
   type = "CNAME"
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "srv_autoconfig" {
@@ -144,6 +155,8 @@ resource "cloudflare_record" "srv_autoconfig" {
     port     = 443
     target   = "autoconfig.lsferreira.net"
   }
+
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "srv_autodiscover" {
@@ -160,6 +173,8 @@ resource "cloudflare_record" "srv_autodiscover" {
     port     = 443
     target   = "autodiscover.lsferreira.net"
   }
+
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "srv_smtp" {
@@ -176,6 +191,8 @@ resource "cloudflare_record" "srv_smtp" {
     port     = 465
     target   = "smtp.lsferreira.net"
   }
+
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "srv_pop3" {
@@ -192,6 +209,8 @@ resource "cloudflare_record" "srv_pop3" {
     port     = 110
     target   = "pop3.lsferreira.net"
   }
+
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "srv_pop3s" {
@@ -208,6 +227,8 @@ resource "cloudflare_record" "srv_pop3s" {
     port     = 995
     target   = "pop3.lsferreira.net"
   }
+
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "srv_imap" {
@@ -224,6 +245,8 @@ resource "cloudflare_record" "srv_imap" {
     port     = 143
     target   = "imap.lsferreira.net"
   }
+
+  tags = [ "terraform" ]
 }
 
 resource "cloudflare_record" "srv_imaps" {
@@ -240,6 +263,8 @@ resource "cloudflare_record" "srv_imaps" {
     port     = 993
     target   = "imap.lsferreira.net"
   }
+
+  tags = [ "terraform" ]
 }
 
 
@@ -252,4 +277,6 @@ resource "cloudflare_record" "webmail" {
   name = "webmail"
   value = "lu-shared04.cpanelplatform.com"
   type = "CNAME"
+
+  tags = [ "terraform" ]
 }
