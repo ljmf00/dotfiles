295cb5f1504bea508dad98b907805f2150e72170
Luis Ferreira <contact@lsferreira.net>
2024-10-21 00:03:04 +0100

    chore(terraform): update email routing

--- terraform/email_routing.tf
+++ terraform/email_routing.tf
@@ -1,19 +1,4 @@
-resource "cloudflare_email_routing_settings" "luisf_eu_org" {
-  zone_id = data.cloudflare_zones.luisf_eu_org.zones[0].id
+resource "cloudflare_email_routing_settings" "lsferreira_net" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
   enabled = "true"
-}
-
-resource "cloudflare_email_routing_catch_all" "luisf_eu_org" {
-  zone_id = data.cloudflare_zones.luisf_eu_org.zones[0].id
-  name    = "catch all"
-  enabled = true
-
-  matcher {
-    type = "all"
-  }
-
-  action {
-    type  = "forward"
-    value = ["catchall@lsferreira.net"]
-  }
 }
