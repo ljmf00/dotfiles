d45840cb552e2694d7d45025ef2afac8deb835ca
Luis Ferreira <contact@lsferreira.net>
2024-10-21 06:49:10 +0100

    chore(terraform): add more DNS entries

--- terraform/records_verify.tf
+++ terraform/records_verify.tf
@@ -3,6 +3,8 @@ resource "cloudflare_record" "txt_protonmail_verify" {
   name = "lsferreira.net"
   value = "protonmail-verification=70cccbdc7c9d67117259cae0540686df67aa782f"
   type = "TXT"
+
+  comment = "terraform"
 }
 
 resource "cloudflare_record" "txt_google_verify" {
@@ -10,6 +12,8 @@ resource "cloudflare_record" "txt_google_verify" {
   name = "lsferreira.net"
   value = "google-site-verification=GDP0s9B0oHyrvGkd95NkXaNLRJvzC49CYLSzCHJyEaw"
   type = "TXT"
+
+  comment = "terraform"
 }
 
 resource "cloudflare_record" "txt_keybase_verify" {
@@ -17,6 +21,8 @@ resource "cloudflare_record" "txt_keybase_verify" {
   name = "lsferreira.net"
   value = "keybase-site-verification=V_PHY7UrKUjJ0V0NpUfi-uJBTYS-UBZCtScmP65VLyI"
   type = "TXT"
+
+  comment = "terraform"
 }
 
 resource "cloudflare_record" "txt_brave_verify" {
@@ -24,4 +30,24 @@ resource "cloudflare_record" "txt_brave_verify" {
   name = "lsferreira.net"
   value = "brave-ledger-verification=cd8be7b9dafc3349d08c6adff904af7629173c8b42ccff04926f3317c0da823a"
   type = "TXT"
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "txt_ovhcontrol_verify" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "ovhcontrol.lsferreira.net"
+  value = "aQUwzlKMuQ4XvI2nW76rxA"
+  type = "TXT"
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "txt_ovhcontrol_status_verify" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "ovhcontrol.status.lsferreira.net"
+  value = "aQUwzlKMuQ4XvI2nW76rxA"
+  type = "TXT"
+
+  comment = "terraform"
 }
--- terraform/records_web.tf
+++ terraform/records_web.tf
@@ -1,10 +1,31 @@
+resource "cloudflare_record" "root_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "lsferreira.net"
+  value = "ft-s-lu-04._.lsferreira.net"
+  type = "CNAME"
+  proxied = true
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "www_root_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "www.lsferreira.net"
+  value = "ft-s-lu-04._.lsferreira.net"
+  type = "CNAME"
+  proxied = true
+
+  comment = "terraform"
+}
+
 resource "cloudflare_record" "s_lsferreira" {
   zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
   name = "s.lsferreira.net"
   value = "nc-anx04-1._.lsferreira.net"
   type = "CNAME"
   proxied = true
-  allow_overwrite = true
+
+  comment = "terraform"
 }
 
 resource "cloudflare_record" "rss_lsferreira" {
@@ -13,7 +34,8 @@ resource "cloudflare_record" "rss_lsferreira" {
   value = "ft-s-lu-04._.lsferreira.net"
   type = "CNAME"
   proxied = true
-  allow_overwrite = true
+
+  comment = "terraform"
 }
 
 resource "cloudflare_record" "social_lsferreira" {
@@ -22,7 +44,8 @@ resource "cloudflare_record" "social_lsferreira" {
   value = "ft-s-lu-04._.lsferreira.net"
   type = "CNAME"
   proxied = true
-  allow_overwrite = true
+
+  comment = "terraform"
 }
 
 resource "cloudflare_record" "feed_lsferreira" {
@@ -31,5 +54,84 @@ resource "cloudflare_record" "feed_lsferreira" {
   value = "ft-s-lu-04._.lsferreira.net"
   type = "CNAME"
   proxied = true
-  allow_overwrite = true
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "inventory_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "inventory.lsferreira.net"
+  value = "ft-s-lu-04._.lsferreira.net"
+  type = "CNAME"
+  proxied = true
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "metrics_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "metrics.lsferreira.net"
+  value = "nc-anx04-1._.lsferreira.net"
+  type = "CNAME"
+  proxied = true
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "my_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "my.lsferreira.net"
+  value = "nc-anx04-1._.lsferreira.net"
+  type = "CNAME"
+  proxied = true
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "status_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "status.lsferreira.net"
+  value = "statuspage.betteruptime.com"
+  type = "CNAME"
+  proxied = false
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "epg_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "epg.lsferreira.net"
+  value = "r.forwarddomain.net"
+  type = "CNAME"
+  proxied = false
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "epg_txt_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "_.epg.lsferreira.net"
+  value = "forward-domain=https://epgshare01.online/epgshare01/epg_ripper_ALL_SOURCES1.xml.gz"
+  type = "TXT"
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "iptv_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "iptv.lsferreira.net"
+  value = "r.forwarddomain.net"
+  type = "CNAME"
+  proxied = false
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "iptv_txt_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "_.iptv.lsferreira.net"
+  value = "forward-domain=https://iptv-org.github.io/iptv/index.m3u"
+  type = "TXT"
+
+  comment = "terraform"
 }
