4f70a09fab98f630c899f5ed070e1b7dc242a1ed
Luis Ferreira <contact@lsferreira.net>
2024-10-21 03:23:19 +0100

    feat(terraform): add infisical provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,21 @@
 terraform {
   required_providers {
+    infisical = {
+      source = "infisical/infisical"
+      version = "0.11.6"
+    }
     cloudflare = {
       source = "cloudflare/cloudflare"
-      version = "~> 4.8.0"
+      version = "4.44.0"
     }
   }
 }
 
+provider "infisical" {
+  client_id = "${var.infisical_client_id}"
+  client_secret = "${var.infisical_client_secret}"
+}
+
 provider "cloudflare" {
-  api_token = "${var.API_CLOUDFLARE_TOKEN_TF}"
+  api_token = data.infisical_secrets.common_secrets.secrets["API_CLOUDFLARE_TOKEN_TERRAFORM"].value
 }
