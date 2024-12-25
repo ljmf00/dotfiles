b4b8658135f8e5419430619fda37f95b136a483d
Luis Ferreira <contact@lsferreira.net>
2024-12-25 23:55:11 +0000

    feat(nix): add kitty shortcut

--- nix/modules/home/terminal/kitty.nix
+++ nix/modules/home/terminal/kitty.nix
@@ -5,4 +5,23 @@
     enable = true;
     extraConfig = lib.fileContents ./../../../../dots/kitty/.config/kitty/kitty.conf;
   };
+
+  dconf.settings = {
+    "org.gnome.desktop.default-applications.terminal" = {
+      exec = "kitty";
+      exec-arg = "";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Control><Alt>T";
+      command = "kitty";
+      name = "Open 'kitty' Terminal";
+    };
+  };
 }
