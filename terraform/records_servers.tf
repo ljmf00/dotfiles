3634c52938981586cda75c3182ecfa5af07ad5e0
Luis Ferreira <contact@lsferreira.net>
2025-03-05 10:58:56 +0000

    chore(terraform): add dh-p-de_frkt-01._.lsferreira.net server

--- terraform/records_servers.tf
+++ terraform/records_servers.tf
@@ -47,3 +47,23 @@ resource "cloudflare_record" "dh_p_de_frkt_02_aaaa_lsferreira" {
 
   comment = "terraform"
 }
+
+resource "cloudflare_record" "dh_p_de_frkt_01_a_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "dh-p-de_frkt-01._.lsferreira.net"
+  value = "212.115.124.73"
+  type = "A"
+  proxied = false
+
+  comment = "terraform"
+}
+
+resource "cloudflare_record" "dh_p_de_frkt_01_aaaa_lsferreira" {
+  zone_id = data.cloudflare_zones.lsferreira_net.zones[0].id
+  name = "dh-p-de_frkt-01._.lsferreira.net"
+  value = "2a05:bec0:34:49bb:cafe:ffff:ffff:1"
+  type = "AAAA"
+  proxied = false
+
+  comment = "terraform"
+}
